1e9c97e726e506fe8363c2c44068b482475312bb
Gurpreet Singh <parasrockz81@gmail.com>
2025-03-26 23:15:06 -0600

    Explaination of Jarvis March Algorithm

--- src/JarvisMarchAlgorithm.cpp
+++ src/JarvisMarchAlgorithm.cpp
@@ -55,6 +55,25 @@ int distance(pair<int,int> a, pair<int,int> b) {
     else return CL;
 }
 
+/*
+* @brief This function implements the Jarvis March algorithm.
+
+    1. Find the leftmost coordinate from the given set of points
+        1.1 Can use the smallest x coordinate to find that the smaller it is 
+            the more left it is.
+    2. Now take that point which we name that as onConvexHull and other point
+       named nextPoint and iterate through the vector of points.
+       2.1 Calculate orientation in this way onConvexHull, nextPoint and 
+           i point which is in interation and calclate its orientation
+           if we get counter clockwise it means that i point can be connected
+           with onConvexHull and cover that nextPoint too. And assign that i
+           to nextPoint so that it may find other one.
+       2.2 If it is collinear it is better to include the farthest point from
+           onConvexHull such that it becomes the minimum number of points.
+    3. If we found the next point equals to firstPoint then simple break
+       and return the result.
+*/
+
 set<pair<int,int>> jarvisMarchFunction(vector<pair<int,int> >& points) {
     
     pair<int,int> onConvexHull = *min_element(points.begin(), points.end(),
