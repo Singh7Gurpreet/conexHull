e234f270fbb331f614fa01f924765ac3f03fa878
Gurpreet Singh <parasrockz81@gmail.com>
2025-03-30 12:43:31 -0600

    Graham algorithm working now

--- src/GrahamScanAlgorithm.cpp
+++ src/GrahamScanAlgorithm.cpp
@@ -8,6 +8,7 @@
 #include "JarvisMarchAlgorithm.cpp"
 
 // global variable that stores the point with minimum y-coordinates
+// in case of ties select with one with leftmost x coordinate
 pair<int,int> p0;
 
 
@@ -16,8 +17,8 @@ struct Comparator {
   bool operator()(pair<int,int>& a, pair<int,int>& b) {
     Orientation calculated = calculateOrientation(p0,a,b);
 
-    if(calculated == CW) return false;
-    else if(calculated == CCW) return true;
+    if(calculated == CCW) return true;
+    else if(calculated == CW) return false;
     // if it is collinear
     else {
       return distance(p0,a) < distance(p0,b);
@@ -61,23 +62,43 @@ pair<int,int> nextToTop(stack<pair<int,int>>& buffer) {
 set<pair<int,int>> GrahamScanFunction(vector<pair<int,int>>& points) {
   int t = findMin(points);
   p0 = points[t];
-  swap(points[0],points[t]);
-  sort(points.begin()+1,points.end(),Comparator());
+  swap(points[0], points[t]);
+
+  sort(points.begin() + 1, points.end(), Comparator());
+
+  vector<pair<int,int>> uniquePoints;
+  uniquePoints.push_back(points[0]);
+
+  for (int i = 1; i < points.size(); ++i) {
+    while (i < points.size() - 1 &&
+           calculateOrientation(p0, points[i], points[i + 1]) == CL)
+      i++;
+    uniquePoints.push_back(points[i]);
+  }
+
+  if (uniquePoints.size() < 3)
+    return {}; // Convex hull not possible
+
   stack<pair<int,int>> hull;
-  for(auto i : points) {
-    while(hull.size() > 1 && calculateOrientation(nextToTop(hull),hull.top(),i) != CW) {
+  hull.push(uniquePoints[0]);
+  hull.push(uniquePoints[1]);
+  hull.push(uniquePoints[2]);
+
+  for (int i = 3; i < uniquePoints.size(); ++i) {
+    while (hull.size() > 1 &&
+           calculateOrientation(nextToTop(hull), hull.top(), uniquePoints[i]) != CCW)
       hull.pop();
-    }
-    hull.push(i);
+    hull.push(uniquePoints[i]);
   }
 
   set<pair<int,int>> result;
-  while(!hull.empty()) {
+  while (!hull.empty()) {
     result.insert(hull.top());
     hull.pop();
   }
 
   return result;
 }
+
 
 #endif
